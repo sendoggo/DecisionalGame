8a57e1208e433178fb7ce729c39203b51583269c
sendoggo <32363390+sendoggo@users.noreply.github.com>
2017-11-20 17:22:02 +0000

    fg

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Francesco
+ */
 public class Main {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        DBConnect connect = new DBConnect();
-        connect.getData();
+        // TODO code application logic here
     }
+    
 }
